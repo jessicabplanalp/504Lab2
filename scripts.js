f2f810a7748c21962699fa725733460ebf65d2ec
Jessica Abplanalp <57146920+jessicabplanalp@users.noreply.github.com>
2020-02-06 23:05:42 -0800

    Update and rename javascript.js to scripts.js

--- scripts.js
+++ scripts.js
@@ -9,9 +9,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_
 
 var control = L.Routing.control({
     waypoints: [
-        L.latLng(47.246587, -122.438830),
-        L.latLng(47.318017, -122.542970),
-        L.latLng(47.258024, -122.444725)
+      null
+        //L.latLng(47.246587, -122.438830),
+        //L.latLng(47.318017, -122.542970),
+        //L.latLng(47.258024, -122.444725)
     ],
      routeWhileDragging: true,
       router: L.Routing.mapbox('pk.eyJ1IjoiamVzc2ljYWJwbGFuYWxwIiwiYSI6ImNrMndvN2g0cjBoam0zYmw2eXd0bm12bTcifQ.RNICUHzCzEvRkIPxzq_RBg'),
@@ -20,4 +21,32 @@ var control = L.Routing.control({
       geocoder: L.Control.Geocoder.mapbox('pk.eyJ1IjoiamVzc2ljYWJwbGFuYWxwIiwiYSI6ImNrMndvN2g0cjBoam0zYmw2eXd0bm12bTcifQ.RNICUHzCzEvRkIPxzq_RBg'),
 
 }).addTo(map);
+//Step 5: Add waypoints by clicking on the map
+function createButton(label, container) {
+    var btn = L.DomUtil.create('button', '', container);
+    btn.setAttribute('type', 'button');
+    btn.innerHTML = label;
+    return btn;
+}
 
+map.on('click', function(e) {
+    var container = L.DomUtil.create('div'),
+        startBtn = createButton('Start from this location', container),
+        destBtn = createButton('Go to this location', container);
+
+        L.DomEvent.on(startBtn, 'click', function() {
+    control.spliceWaypoints(0, 1, e.latlng);
+    map.closePopup();
+});
+
+L.DomEvent.on(destBtn, 'click', function() {
+    control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+    control.show();
+    map.closePopup();
+});
+
+    L.popup()
+        .setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(map);
+ });
